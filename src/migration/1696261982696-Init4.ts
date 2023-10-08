1fa790e45d2e9d72be03e9864f8b8c2e4830801f
infeibal <theliamegames@gmail.com>
2023-10-08 16:52:51 +0700

    fix migrations

--- src/migration/1696261982696-Init4.ts
+++ src/migration/1696261982696-Init4.ts
@@ -1,16 +1,16 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+// import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init41696261982696 implements MigrationInterface {
-    name = 'Init41696261982696'
+// export class Init41696261982696 implements MigrationInterface {
+//     name = 'Init41696261982696'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`news\` ADD \`photos\` text NULL`);
-        await queryRunner.query(`ALTER TABLE \`news\` CHANGE \`createdAt\` \`createdAt\` int NOT NULL DEFAULT (UNIX_TIMESTAMP())`);
-    }
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE \`news\` ADD \`photos\` text NULL`);
+//         await queryRunner.query(`ALTER TABLE \`news\` CHANGE \`createdAt\` \`createdAt\` int NOT NULL DEFAULT (UNIX_TIMESTAMP())`);
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`news\` CHANGE \`createdAt\` \`createdAt\` int NOT NULL DEFAULT 'unix_timestamp()'`);
-        await queryRunner.query(`ALTER TABLE \`news\` DROP COLUMN \`photos\``);
-    }
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE \`news\` CHANGE \`createdAt\` \`createdAt\` int NOT NULL DEFAULT 'unix_timestamp()'`);
+//         await queryRunner.query(`ALTER TABLE \`news\` DROP COLUMN \`photos\``);
+//     }
 
-}
+// }
---
+++ src/migration/1696758734559-Init6.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init61696758734559 implements MigrationInterface {
+    name = 'Init61696758734559'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`users\` ADD \`profile_picture\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`news\` CHANGE \`createdAt\` \`createdAt\` int NOT NULL DEFAULT (UNIX_TIMESTAMP())`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`news\` CHANGE \`createdAt\` \`createdAt\` int NOT NULL DEFAULT 'unix_timestamp()'`);
+        await queryRunner.query(`ALTER TABLE \`users\` DROP COLUMN \`profile_picture\``);
+    }
+
+}
